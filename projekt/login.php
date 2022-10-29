9e7c826cec4989bf7662d523d06542ece1d7ac6e
Tutislav <tutislav@outlook.cz>
2022-10-29 19:56:07 +0200

    Update index.php, login.php and register.php

--- projekt/login.php
+++ projekt/login.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Přihlášení - IT World</title>
     <link href="casopis.css" rel="stylesheet">
 	<link href="login.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-
     <div class="container">
         <div id="login_register">
             <div class="home"><a href="index.html"><i class="fa fa-home" style="font-size:30px"></i> </a></div>
         </div>
-
         <div class="login">
-		<h2>Přihlášení uživatele</h2>
+		    <h2>Přihlášení uživatele</h2>
             <form action="backend/login.php" method="POST">
                 <i class="fa fa-envelope" style="font-size:24px"></i><input type="email" placeholder="Email" id="mail" name="mail"><br>
                 <i class="fa fa-lock" style="font-size:24px"></i><input type="password" placeholder="Heslo" id="password" name="password"><br>
                 <input type="submit" value="Přihlásit se   " id="login-submit">
             </form>
-
         </div>
-
 
 	<?php
 		session_start();
@@ -36,6 +32,5 @@
 		}
 	?>
     </div>
-    
 </body>
 </html>
--- projekt/register.php
+++ projekt/register.php
@@ -1,37 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registrace - IT World</title>
     <link href="casopis.css" rel="stylesheet">
 	<link href="register.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-aweso me/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div class="container">
         <div id="login_register">
             <div class="home"><a href="index.html"><i class="fa fa-home" style="font-size:30px"></i></a></div>
-            <span id="login"><a href="login.html">PŘIHLÁŠENÍ</a></span>
-            <span id="register"><a href="registration.html">REGISTRACE</a></span>
         </div>
-
         <div class="registration">
 		    <h2>Registrace uživatele</h2>
             <form action="backend/register.php" method="POST">
                 <i class="fa fa-envelope" style="font-size:24px"></i><input type="email" placeholder="Email" id="mail" name="mail"><br>
                 <i class="fa fa-lock" style="font-size:24px"></i><input type="password" placeholder="Heslo" id="password" name="password"><br>
                 <i class="fa fa-lock" style="font-size:24px"></i><input type="password" placeholder="Potvrzení hesla" id="password-confirm" name="password_confirm"><br>
-				
                 <input type="text" placeholder="Jméno" id="name" name="name"><br>
                 <input type="tel"  placeholder="Příjmení" id="lastname" name="lastname"><br>
-
                 <input type="submit" value="Zaregistrovat   " id="register-submit">
             </form>
-
         </div>
-
 
 	<?php
 		session_start();
