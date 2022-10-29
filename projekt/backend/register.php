0409ffff0c0aa0561d38aff5bdbc18488fcd3b1e
GRANDfaceStudent <matl07@student.vspj.cz>
2022-10-29 17:36:19 +0200

    Update register.php

--- projekt/backend/register.php
+++ projekt/backend/register.php
@@ -9,13 +9,17 @@
 	$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 	$role = "redaktor";
 	
-	//Insert--------
 	if(empty($name) || empty($last_name)) 
 	{
-		header("../index.html");
+		header("Location: ../index.html");
 	}
-	$insert = "insert into uzivatel (jmeno, prijmeni, email, heslo, role) values('$name', '$last_name', '$email', '$password', '$role')";
-	$result = mysqli_query($conn, $insert);
-	if(!$result) { die("Nepodařilo se přihlásit!"); }
-	else { echo "Registrace byla úspěšná."; }
+	else
+	{
+		//Insert--------
+		$insert = "insert into uzivatel (jmeno, prijmeni, email, heslo, role) values('$name', '$last_name', '$email', '$password', '$role')";
+		$result = mysqli_query($conn, $insert);
+		if(!$result) { die("Nepodařilo se přihlásit!"); }
+		else { echo "Registrace byla úspěšná."; }
+	}
+	
 ?>
