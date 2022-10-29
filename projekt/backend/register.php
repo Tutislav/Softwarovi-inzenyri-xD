b37bfdc05d4f0f661967b4d33f026d5d99b2346a
GRANDfaceStudent <matl07@student.vspj.cz>
2022-10-29 17:40:46 +0200

    Update register.php

--- projekt/backend/register.php
+++ projekt/backend/register.php
@@ -10,7 +10,7 @@
 	$role = "redaktor";
 	
 	//Empty check--------
-	if(empty($name) || empty($last_name) || $email) 
+	if(empty($name) || empty($last_name) || empty($email)) 
 	{ 
 		header("Location: ../registration.html"); 
 	}
