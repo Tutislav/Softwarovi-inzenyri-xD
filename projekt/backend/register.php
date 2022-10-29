2bc86deae0f42e5a0d85ba859de414cdb032c472
GRANDfaceStudent <matl07@student.vspj.cz>
2022-10-29 17:29:31 +0200

    Empty php update

--- projekt/backend/register.php
+++ projekt/backend/register.php
@@ -10,7 +10,10 @@
 	$role = "redaktor";
 	
 	//Insert--------
-	if(empty($name) || empty($last_name)) { echo "Jmeno uzivatele nebo prijmeni je prazdne."; }
+	if(empty($name) || empty($last_name)) 
+	{
+		header(../index.html);
+	}
 	$insert = "insert into uzivatel (jmeno, prijmeni, email, heslo, role) values('$name', '$last_name', '$email', '$password', '$role')";
 	$result = mysqli_query($conn, $insert);
 	if(!$result) { die("Nepodařilo se přihlásit!"); }
