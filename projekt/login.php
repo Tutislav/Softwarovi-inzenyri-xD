f6e4972649d21fb5665c3ba69e1797a5b2d4fef4
Tutislav <tutislav@outlook.cz>
2022-11-01 22:37:06 +0100

    Update login.php and register.php

--- projekt/login.php
+++ projekt/login.php
@@ -1,3 +1,12 @@
+<?php
+    //Session start--------
+    session_start();
+    //Messages--------
+    if (isset($_SESSION["error"])) {
+        $message = $_SESSION["error"];
+        unset($_SESSION["error"]);
+    }
+?>
 <!DOCTYPE html>
 <html lang="cs">
 <head>
@@ -8,33 +17,27 @@
     <link href="casopis.css" rel="stylesheet">
 	<link href="login.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#message").fadeOut(5000);
+        });
+    </script>
 </head>
 <body>
-	<?php
-		//Session start--------
-		session_start();
-	?>
     <div class="container">
         <div id="login_register">
+            <span id="message"><?= $message ?></span>
             <div class="home"><a href="/"><i class="fa fa-home"></i></a></div>
         </div>
         <div class="login">
 		    <h2>Přihlášení uživatele</h2>
             <form action="backend/login.php" method="POST">
-                <label for="mail" class="fa fa-envelope"></label><input type="email" placeholder="Email" id="mail" name="mail"><br>
-                <label for="password" class="fa fa-lock"></label><input type="password" placeholder="Heslo" id="password" name="password"><br>
+                <label for="mail" class="fa fa-envelope"></label><input type="email" placeholder="Email" id="mail" name="mail" required><br>
+                <label for="password" class="fa fa-lock"></label><input type="password" placeholder="Heslo" id="password" name="password" required><br>
                 <input type="submit" value="Přihlásit se">
             </form>
         </div>
-
-	<?php
-		//Session check--------
-		if(isset($_SESSION["error"]))
-		{
-			echo $_SESSION["error"];
-			unset($_SESSION["error"]);
-		}
-	?>
     </div>
 </body>
 </html>
