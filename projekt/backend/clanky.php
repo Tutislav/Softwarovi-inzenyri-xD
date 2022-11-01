aa720132da9674a793b612d0d0687f8f13725cdf
Tutislav <tutislav@outlook.cz>
2022-11-01 22:51:55 +0100

    Update clanky.php

--- projekt/backend/clanky.php
+++ projekt/backend/clanky.php
@@ -1,51 +1,50 @@
 <?php
-	//Session start--------
-	session_start();
+    //Session start--------
+    session_start();
     //Logout--------
     if (isset($_GET["logout"])) {
         session_destroy();
         header("Location: /backend/clanky.php");
     }
+    //Login and register--------
+    if (!isset($_SESSION["email"])) {
+        $login_span = "<a href='login.php'>PŘIHLÁŠENÍ</a>";
+        $register_span = "<a href='register.php'>REGISTRACE</a>";
+    }
+    else {
+        $login_span = $_SESSION["email"];
+        $register_span = "<a href='/backend/clanky.php?logout'>ODHLÁSIT SE</a>";
+    }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Články</title>
+    <title>IT World - Články</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="../casopis.css" rel="stylesheet">
-	<link href="clanek.css" rel="stylesheet">	
+	<link href="clanek.css" rel="stylesheet">
 	
 </head>
 <body>
     <div class="container">
         <div id="login_register">
-            <span id="login">
-                <?php
-                    if (!isset($_SESSION["email"])) echo("<a href='login.php'>PŘIHLÁŠENÍ</a>");
-                    else echo($_SESSION["email"]);
-                ?>
-            </span>
-            <span id="register">
-                <?php
-                    if (!isset($_SESSION["email"])) echo("<a href='register.php'>REGISTRACE</a>");
-                    else echo("<a href='/backend/clanky.php?logout'>ODHLÁSIT SE</a>")
-                ?>
-            </span>
+            <span id="login"><?= $login_span ?></span>
+            <span id="register"><?= $register_span ?></span>
         </div>
         <div id="heading">
             <h1>IT WORLD</h1>
         </div>
         <div id="navigation">
-                <ul> 
-                    <li><a href="../">ÚVOD</a></li>
-                    <li><a href="">ČLÁNKY</a></li>
-                    <li><a href="">ARCHIV</a></li>
-                    <li class="kontakt"><a href="">KONTAKT</a></li>
-                    <li class="helpdesk"><a href="">HELPDESK</a></li>
-                </ul>
+            <ul> 
+                <li><a href="/">ÚVOD</a></li>
+                <li><a href="">ČLÁNKY</a></li>
+                <li><a href="">ARCHIV</a></li>
+                <li class="kontakt"><a href="">KONTAKT</a></li>
+                <li class="helpdesk"><a href="">HELPDESK</a></li>
+            </ul>
         </div>
 	    
         <div id="clankyList">
