3a0c00e491a1ea874dd1e6dacec0143fab894bda
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-03 19:25:52 +0100

    Update add_article.html & backend/add_article.php

--- projekt/backend/add_article.php
+++ projekt/backend/add_article.php
@@ -1,6 +1,12 @@
 <?php
+	//Session--------
+	require("common.php");
+
 	//Variables--------
-	
+	$title = $_POST["title"];
+	$authors = $_POST["authors"];
+	$file = $_POST["file"];
+	$theme = $_POST["theme"];
 	
 	//Connect--------
 ?>
