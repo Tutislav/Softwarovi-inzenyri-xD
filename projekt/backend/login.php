d84893582f24b98476b5b233c242a4768435caba
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-05 08:56:40 +0100

    Update login.php

--- projekt/backend/login.php
+++ projekt/backend/login.php
@@ -2,7 +2,7 @@
 	//Session--------
 	session_start();
 
-	//Connect--------
+	//Connect database--------
 	require("connect.php");
 
 	//Variables--------
@@ -10,30 +10,40 @@
 	$password = $_POST["password"];
 	$user_exists = false;
 
-	//Select--------
-	$select = "select * from uzivatel";
-	$result = mysqli_query($conn, $select);
-	if($result)
+	//Empty check--------
+	if(!empty($email) && !empty($password))
 	{
-		foreach($result as $item)
+		//Select database--------
+		$select = "select * from uzivatel";
+		$result = mysqli_query($conn, $select);
+		if($result)
 		{
-			if($email == $item["email"] && password_verify($password, $item["heslo"]))
+			foreach($result as $item)
 			{
-				$user_exists = true;
-				$_SESSION["message"] = "Přihlášení bylo úspěšné.";
-				$_SESSION["user_id"] = $item["id_uzivatele"];
-				$_SESSION["name"] = $item["jmeno"];
-				$_SESSION["last_name"] = $item["prijmeni"];
-				$_SESSION["email"] = $item["email"];
-				$_SESSION["role"] = $item["role"];
-				header("Location: ../index.php");
-				break;
+				if($email == $item["email"] && password_verify($password, $item["heslo"]))
+				{
+					$user_exists = true;
+					$_SESSION["message"] = "Přihlášení bylo úspěšné.";
+					$_SESSION["user_id"] = $item["id_uzivatele"];
+					$_SESSION["name"] = $item["jmeno"];
+					$_SESSION["last_name"] = $item["prijmeni"];
+					$_SESSION["email"] = $item["email"];
+					$_SESSION["role"] = $item["role"];
+					header("Location: ../index.php");
+					break;
+				}
+			}
+			if(!$user_exists)
+			{
+				$_SESSION["message"] = "Nepodařilo se přihlásit.";
+				header("Location: ../login.php");		
 			}
 		}
-		if(!$user_exists)
-		{
-			$_SESSION["message"] = "Nepodařilo se přihlásit.";
-			header("Location: ../login.php");		
-		}
 	}
+	else 
+	{
+		$_SESSION["message"] = "Vyplňte všechna políčka.";
+		header("Location: ../login.php");
+	}
+	
 ?>
