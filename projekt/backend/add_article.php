cd9dd21b4b8247f7920d639d079b0050057b073d
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-05 16:35:34 +0100

    Update add_article.php

--- projekt/backend/add_article.php
+++ projekt/backend/add_article.php
@@ -9,28 +9,30 @@
 	$title = $_POST["title"];
 	$authors = $_POST["authors"];
 	$theme = $_POST["theme"];
-	$file_name = $_FILES["file"]["name"];
-	
+	$file = $_FILES["file"]["name"];
 
 	//Empty check--------
-	if(!empty($title) && !empty($authors) && !empty($file_name) && !empty($theme))
+	if(!empty($title) && !empty($authors) && !empty($file) && !empty($theme))
 	{		
 		//File--------
+		$file_array = explode(".", $file);
+		$file_name = $file_array[0];
+		$file_ext = $file_array[1];
+		$file_name = $file_name."_".date("y-m-d-h-i-s").".".$file_ext;
+
 		$file_loc = "../clanky/".$file_name;
 		$file_name_temp = $_FILES["file"]["tmp_name"];
 		if(move_uploaded_file($file_name_temp, $file_loc)) 
 		{ 
 			//Insert database soubor--------
+			
 			//Insert database prispevek--------	
 		}
 		else 
 		{
 			$_SESSION["message"] = "Nepodařilo se poslat soubor článku.";
 			header("Location: ../add_article.php");
-		}
-		
-		//Insert database
-		
+		}	
 	}
 	else 
 	{ 
