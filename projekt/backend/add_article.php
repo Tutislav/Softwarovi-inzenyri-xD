68538d0decf906b8e5ba3460794b3080894e9eae
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-05 08:36:38 +0100

    Update add_article.php

--- projekt/backend/add_article.php
+++ projekt/backend/add_article.php
@@ -10,11 +10,14 @@
 	$authors = $_POST["authors"];
 	$theme = $_POST["theme"];
 	$file_name = $_FILES["file"]["name"];
-	$file_loc = "clanky/".$file_name;
-	$file_name_temp = $_FILES["file"]["tmp_name"];
+	
 
 	//Insert--------
-		
+	if(!empty($title) && !empty($authors) && !empty($file_name) && !empty($theme))
+	{
+
+	}
+	else { echo "neco chybi"; }
 
 	//File upload--------
 	//if(move_uploaded_file($file_name_temp, $file_loc)) { echo "Článek byl úspěšně poslán."; }
