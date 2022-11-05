17bbba461453f07e23451322f709e848357d542d
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-05 08:31:31 +0100

    Update add_article.php

--- projekt/backend/add_article.php
+++ projekt/backend/add_article.php
@@ -6,16 +6,14 @@
 	$title = $_POST["title"];
 	$authors = $_POST["authors"];
 	$theme = $_POST["theme"];
+	$file	
 
 	//File upload--------
 	$file_name = $_FILES["file"]["name"];
-	$file_loc = "/home/itworld/clanky/".$file_name;
+	$file_loc = "clanky/".$file_name;
 	$file_name_temp = $_FILES["file"]["tmp_name"];
-	echo $file_name . "<br>";
-	echo $file_loc . "<br>";
-	echo $file_name_temp . "<br>";
 	if(move_uploaded_file($file_name_temp, $file_loc)) { echo "Článek byl úspěšně poslán."; }
-	else { echo "Nepodařilo se poslat článek."; }	
+	else { echo $_FILES["file"]["error"]; }	
 
 	//Connect--------
 	require("connect.php");
