0f8765e36f9ef801a393700aabaf405250c4c322
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-05 15:56:13 +0100

    Update add_article.php

--- projekt/backend/add_article.php
+++ projekt/backend/add_article.php
@@ -16,14 +16,10 @@
 	if(!empty($title) && !empty($authors) && !empty($file_name) && !empty($theme))
 	{		
 		//Insert database
-		$insert = "insert into prispevek (id_prispevku, id_uzivatele, tematicke_cislo, spoluautori, stav, titulek)
-			values (1, $_SESSION[user_id], $theme, $authors, Schváleno, $title)";
-		$result = mysqli_query($conn, $insert);
-		if($result) { echo "Zapsání do databáze bylo úspěšné."; }
-		else { echo "Nepodařilo se zapsat do databáze."; }
+		
 		
 		//File--------
-		$file_loc = "clanky/".$file_name;
+		$file_loc = "/clanky/".$file_name;
 		$file_name_temp = $_FILES["file"]["tmp_name"];
 		if(move_uploaded_file($file_name_temp, $file_loc)) { echo "Článek byl úspěšně poslán."; }
 		else { echo "Článek se nepodařilo poslat."; }
