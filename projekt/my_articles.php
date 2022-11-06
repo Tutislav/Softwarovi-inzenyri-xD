bf52e45a56fedc7dceca59629d616e21f716005d
Envedow <115730921+Envedow@users.noreply.github.com>
2022-11-06 22:02:39 +0100

    Update sql query

--- projekt/my_articles.php
+++ projekt/my_articles.php
@@ -52,7 +52,7 @@
 <?php
 	require("backend/connect.php");
 	require("backend/common.php");	
-	$sql = "SELECT id_prispevku, titulek, stav FROM prispevek NATURAL JOIN uzivatel";
+	$sql = "SELECT id_prispevku, titulek, stav FROM prispevek NATURAL JOIN uzivatel WHERE uzivatel.email='" . $_SESSION["email"] . "'";
 			
 	$result = $conn->query($sql);
 			
