5e52b6267cce905d551bbeef2d81118e041d53f0
Envedow <115730921+Envedow@users.noreply.github.com>
2022-11-06 22:25:56 +0100

    Update my_articles.php

--- projekt/my_articles.php
+++ projekt/my_articles.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moje článeky</title>
+    <title>Moje články</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="casopis.css" rel="stylesheet">
 	<link href="css/my_articles.css" rel="stylesheet">
