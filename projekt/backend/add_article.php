f4f5f94c8e4f6df19e1e072d08a81466c54a0fed
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-07 11:14:03 +0100

    Update add_article.php

--- projekt/backend/add_article.php
+++ projekt/backend/add_article.php
@@ -35,7 +35,7 @@
 				
 				$select = "select id_prispevku from prispevek order by id_prispevku desc limit 1";
 				$last_article_id = mysqli_query($conn, $select);
-				echo $last_article_id["id_prispevku"]; 
+				echo $last_article_id[0]["id_prispevku"]; 
 				//if($last_article_id)
 				//{
 				//	$insert = "insert into soubor (id_prispevku, soubor_cesta)
