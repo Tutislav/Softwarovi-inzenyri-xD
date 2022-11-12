9fd4046ed16f05500835822a75f15db690ee0c75
Tutislav <tutislav@outlook.cz>
2022-11-12 21:23:49 +0100

    Update messages.php

--- projekt/messages.php
+++ projekt/messages.php
@@ -40,24 +40,46 @@
                 <li class="helpdesk"><a href="">HELPDESK</a></li>
             </ul>
         </div>
-        <div>
+        <table>
+            <tr>
+                <th>Datum</th>
+                <th>Odesílatel</th>
+                <th>Zpráva</th>
+                <th>Recenze</th>
+                <th>Přečteno</th>
+            </tr>
             <?php
                 require("backend/connect.php");
-                $sql = "SELECT jmeno, prijmeni, vzkaz_text FROM uzivatel JOIN vzkazy ON uzivatel.id_uzivatele=vzkazy.id_odesilatele WHERE id_prijemce='$user_id';";
+                $sql = "SELECT datum_odeslani, jmeno, prijmeni, vzkaz_text, id_prispevku, precteno FROM uzivatel JOIN vzkazy ON uzivatel.id_uzivatele=vzkazy.id_odesilatele NATURAL JOIN recenze WHERE id_prijemce='$user_id';";
                 $result = $conn->query($sql);
                 $conn->close();
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
+                        $date = date("d.m.y h:i", strtotime($row["datum_odeslani"]));
                         $sender_name = $row["jmeno"] . " " . $row["prijmeni"];
                         $message_text = $row["vzkaz_text"];
-                        echo("<p>" . $sender_name ." - " . $message_text . "</p>");
+                        if (!empty($row["id_recenze"])) {
+                            $review = "<button onlick='location.href=clanek.php?id=" . $row["id_prispevku"] . "#recenze'>Recenze</button>";
+                        }
+                        else {
+                            $review = "";
+                        }
+                        if ($row["precteno"]) {
+                            $read = "<input type='checkbox' name='read' id='read' checked>";
+                        }
+                        else {
+                            $read = "<input type='checkbox' name='read' id='read'>";
+                        }
+                        echo("<tr>");
+                        echo("<td>" . $date . "</td><td>" . $sender_name ."</td><td>" . $message_text . "</td><td>" . $review . "</td><td>" . $read . "</td>");
+                        echo("</tr>");
                     }
                 }
                 else {
-                    echo("Žádné vzkazy");
+                    echo("<p>Žádné vzkazy</p>");
                 }
             ?>
-        </div>
+        </table>
     </div>
 </body>
 </html>
