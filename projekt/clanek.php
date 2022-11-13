28a7459e717e0348cc11075c20d9ad10c94c65ae
pelika14 <pelika14@student.vspj.cz>
2022-11-13 19:29:10 +0100

    Update clanek.php

--- projekt/clanek.php
+++ projekt/clanek.php
@@ -165,7 +165,8 @@
 				echo "</div>";
 				echo "<div class='text_recenze' style='display: none'>";
 					echo $row["recenze_text"]."<br>";
-					echo "<button onclick='document.location=review_objection.php/".echo "id=".$row["id_recenze"]."'".">Oponentní formulář</button><br>";
+					//echo "<button onclick='document.location=review_objection.php/".echo "id=".$row["id_recenze"]."'";.">Oponentní formulář</button><br>";
+					echo "<button onclick=".'"document.location='."'review_objection.php?id=" . $row["id_recenze"] ."'".'">'."Oponentní formulář</button>";
 				echo "</div>";
 				echo "</div><br><hr>";
 				$counter_recenze++;
