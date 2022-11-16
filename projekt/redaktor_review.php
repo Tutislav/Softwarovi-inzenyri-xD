eff4d86a7b60535cd36d7474d586effd01653839
MartinVenhoda <115802778+MartinVenhoda@users.noreply.github.com>
2022-11-16 11:58:06 +0100

    Update redaktor_review.php

--- projekt/redaktor_review.php
+++ projekt/redaktor_review.php
@@ -89,11 +89,9 @@
         $conn->close();
         if ($result) {
             $_SESSION["message"] = "Zpráva byla úspěšně odeslána.";
-            header("Location: /redaktor_review.php?id=" );
         }
         else {
             $_SESSION["message"] = "Zprávu nelze odeslat.";
-            header("Location: /redaktor_review.php?id=" );
         }
 
 ?>
