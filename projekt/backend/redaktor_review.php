ba2f60a13e23aa837546ab41f813aefead98cdf8
MartinVenhoda <115802778+MartinVenhoda@users.noreply.github.com>
2022-11-16 12:17:25 +0100

    Update redaktor_review.php

--- projekt/backend/redaktor_review.php
+++ projekt/backend/redaktor_review.php
@@ -5,16 +5,15 @@
         $user_id = $_POST['user_id'];
         $text = $_POST['text'];
         $sql = "INSERT INTO vzkazy (id_odesilatele, id_prijemce, vzkaz_text) VALUES ('$redactor_id', '$user_id', '$text');";
-        echo $sql;
         $result = $conn->query($sql);
         $conn->close();
-        /*if ($result) {
+        if ($result) {
             $_SESSION["message"] = "Zpráva byla úspěšně odeslána.";
             header("Location: /articles_management.php" );
         }
         else {
             $_SESSION["message"] = "Zprávu nelze odeslat.";
             header("Location: /articles_management.php" );
-        }*/
+        }
 
 ?>
