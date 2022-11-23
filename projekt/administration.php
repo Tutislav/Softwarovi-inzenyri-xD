85e4d55a696c5723a19cb88758fec1c061a027c2
Envedow <115730921+Envedow@users.noreply.github.com>
2022-11-23 11:54:15 +0100

    Update administration.php

--- projekt/administration.php
+++ projekt/administration.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IT World</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/administration.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
         $(document).ready(function(){
