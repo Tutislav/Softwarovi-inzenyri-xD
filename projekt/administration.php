82bd2e040c6f783ee419254a5adeb199b1a28fd6
Envedow <115730921+Envedow@users.noreply.github.com>
2022-11-23 11:53:19 +0100

    Create administration.php

---
+++ projekt/administration.php
@@ -0,0 +1,38 @@
+<?php
+    require("backend/common.php");
+?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT World</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#message").fadeIn().fadeOut(10000);
+        });
+    </script>
+</head>
+<body>
+    <div class="container">
+        <div id="login_register">
+            <span id="message"><?= $message ?></span>
+            <span id="login"><?= $login_span ?></span>
+            <span id="register"><?= $register_span ?></span>
+        </div>
+        <div id="navigationMenu">
+            <ul> 
+                <li id="main"><a href="/">Hlavní menu</a></li>
+                <li><a href="/">Správa uživatelů</a></li>
+            </ul>
+        </div>
+        <div id="content">
+            <p>awvda</p>
+        </div>
+    </div>
+</body>
+</html>
