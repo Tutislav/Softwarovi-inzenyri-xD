a4fd0c0ce8f3496ad7a9b63237ea20b0507ecb64
Envedow <115730921+Envedow@users.noreply.github.com>
2022-11-26 16:22:42 +0100

    Update administration.php

--- projekt/administration.php
+++ projekt/administration.php
@@ -23,7 +23,7 @@
                 $("#user_" + $(this).parent().parent().children().first().html()).toggle();
                 $("#user_" + $(this).parent().parent().children().first().html() + "_manage").toggle();
             });
-	    $("#search").change(function(){
+	    $("#search").keyup(function(){
                 $("#searchForm").submit();
             });
         });
