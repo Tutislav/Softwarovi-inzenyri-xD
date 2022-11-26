2b225b64086e4043f67f8e0c9bc38fd98905c5ea
Tutislav <tutislav@outlook.cz>
2022-11-26 18:26:26 +0100

    Fix font issues

--- projekt/administration.php
+++ projekt/administration.php
@@ -116,7 +116,7 @@
                                         			<td>". $row["jmeno"] . " " . $row["prijmeni"] . "</td>
                                        				<td>". $row["email"] . "</td>
                                         			<td>". $row["role"] . "</td>
-                                        			<td><button class='manage'><i class='fa fa-wrench'>Spravovat</button></td>
+                                        			<td><button class='manage'><i class='fa fa-wrench'></i>Spravovat</button></td>
                                       			</tr>";
                   					echo "<tr id='user_" .  $row["id_uzivatele"] . "_manage' style='display: none;'>";
 			   				echo "<td>" . $row["id_uzivatele"] . "</td>";
@@ -130,7 +130,7 @@
                    					echo "<td><select name='role' id='role'>" . $roles . "</select></td>";
                    					echo "<td><button type='submit' name='edit' id='edit'><i class='fa fa-floppy-o'></i>Uložit</button></form>";
                    					echo "<form action='backend/administration.php' method='post'><input type='hidden' name='user_id' id='user_id' value='" . $row["id_uzivatele"] . "'><button type='submit' name='delete' id='delete' onclick='return confirm(\"Opravdu chcete smazat tohoto uživatele?\")'><i class='fa fa-trash'></i>Smazat</button></form>";
-                   					echo "<button class='close'><i class='fa fa-close'>Zavřít</button>";
+                   					echo "<button class='close'><i class='fa fa-close'></i>Zavřít</button>";
                    					echo "</td></tr>";
                 				}
 					}
@@ -170,7 +170,7 @@
                                         			<td><a href='clanek.php?id=" .$row["id_prispevku"]."' target='_blank'>". $row["titulek"] . "</a></td>
                                        				<td>". $row["tematicke_cislo"] . "</td>
                                         			<td>". $row["stav"] . "</td>
-                                        			<td><button class='manage'><i class='fa fa-wrench'>Spravovat</button></td>
+                                        			<td><button class='manage'><i class='fa fa-wrench'></i>Spravovat</button></td>
                                       			      </tr>";
 							echo "<tr id='user_" .  $row["id_prispevku"] . "_manage' style='display: none;'>";
 							
@@ -189,7 +189,7 @@
 							
                    					echo "<td><button type='submit' name='edit' id='edit'><i class='fa fa-floppy-o'></i>Uložit</button></form>";
                    					echo "<form action='backend/administration.php' method='post'><input type='hidden' name='article_id' id='article_id' value='" . $row["id_prispevku"] . "'><button type='submit' name='delete' id='delete' onclick='return confirm(\"Opravdu chcete smazat tento článek?\")'><i class='fa fa-trash'></i>Smazat</button></form>";
-                   					echo "<button class='close'><i class='fa fa-close'>Zavřít</button>";
+                   					echo "<button class='close'><i class='fa fa-close'></i>Zavřít</button>";
                    					echo "</td></tr>";
 						}
 						
