cbe27c93b832e57e644473d9c295ee6c8efae20f
GRANDfaceStudent <matl07@student.vspj.cz>
2022-11-30 14:15:13 +0100

    Update articles_upload.php

--- projekt/articles_upload.php
+++ projekt/articles_upload.php
@@ -55,7 +55,7 @@
 							echo "<td>";
 								echo "<a href='../clanek.php?id=".$item["id_prispevku"]."'>".$item["titulek"]."</a></td>";
 							echo "</td>";
-							echo "<td class='checkbox'>";
+							echo "<td style='text-align:center'>";
 								echo "<input type='checkbox' name='article_checked' value='".$item["id_prispevku"]."' />";
 							echo "</td>";
 						echo "</tr>";
