1654531b160a8ea0185fde84838073a78f4c949f
pelika14 <pelika14@student.vspj.cz>
2022-12-04 20:21:27 +0100

    Asi hotový zadávání recenzí
    asi hotový zadávání recenzí nemusí to zatím funguvat tak jak má a pokud ano tak je to dobrý

--- projekt/backend/actual_review.php
+++ projekt/backend/actual_review.php
@@ -2,15 +2,34 @@
 //action stránka pro recenze
         require("common.php");
         require("connect.php");
-		$sql="INSERT INTO"
-
-        $conn->close();
-       
+		$id_ukolu=$_GET["id_ukolu"];
+		$id_prispevku=$_GET["id_prispevku"];
+		//gety pro data recenze
+		$aktualnost = $_GET["aktualnost"];
+		$originalita = $_GET["originalita"];
+		$odborna_u = $_GET["odborna_u"];
+		$jazykova_u = $_GET["jazykova_u"];
+		$review = $_GET["review"];
+		//*********************
+		$today = date("Y-m-d H:i:s");
+		$msg = "";
+		$sql="UPDATE ukol SET splneno = 1, datum_splneni='".$today."' WHERE id_ukolu='".$id_ukolu."'";//update ukol na splneno = 1 dotaz funguje
+		$result = $conn->query($sql);
+ 
         if ($result) {
-            $_SESSION["message"] = "Zpráva byla úspěšně odeslána.";
+            $msg = "Update byl úspěšný";
         }
         else {
-            $_SESSION["message"] = "Zprávu nelze odeslat.";
+            $msg = "Nepovedl se update.";
         }
-
+		$sql="INSERT INTO recenze VALUES ('".$id_prispevku."', '".$_SESSION["user_id"]."', '".$id_ukolu."','".$aktualnost."','".$originalita."','".$odborna_u."','".$jazykova_u."',0)";//INSERT recenze
+		$result = $conn->query($sql);
+		 if ($result) {
+            $msg = "Insert byl úspěšný";
+        }
+        else {
+            $msg = "Nepovedl se Insert.";
+        }
+		$_SESSION["message"]=$msg;
+		$conn->close();
 ?>
