b8e06f73fb6658d3595940e9fdebc77f01f48474
Envedow <115730921+Envedow@users.noreply.github.com>
2022-12-15 10:50:52 +0100

    Create monitoring.php

---
+++ projekt/monitoring.php
@@ -0,0 +1,155 @@
+<?php
+    $role_restriction = "sefredaktor";
+    require("backend/common.php");
+    if(isset($_POST["search"]))
+	    $search = $_POST["search"];
+    else
+	    $search = "";
+?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT World</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/administration.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <?= $scripts ?>
+    <script>
+        $(document).ready(function(){
+	    var input = $("#search");
+    	    var len = input.val().length;
+            input[0].focus();
+    	    input[0].setSelectionRange(len, len);
+		
+	    function delay(callback, ms) {
+  		var timer = 0;
+  		return function() {
+   			var context = this, args = arguments;
+   			clearTimeout(timer);
+    			timer = setTimeout(function () {
+      				callback.apply(context, args);
+    			}, ms || 0);
+  	        };
+	    }
+	    $("#search").keyup(delay(function(e){
+                $("#searchForm").submit();
+            }, 300));
+        });
+    </script>
+</head>
+<body>
+    <div class="container">
+        <div id="login_register">
+            <span id="message"><?= $message ?></span>
+            <span id="login"><?= $login_span ?></span>
+            <span id="register"><?= $register_span ?></span>
+        </div>
+        <div id="navigationMenu">
+            <ul> 
+                <li id="main"><a href="/"><i class="fa fa-chevron-left"></i>Hlavní stránka</a></li>
+		<li>
+		    <form action="/monitoring.php" method="POST">
+			<input type="hidden" name="contentChange" value="1">
+			<i class="fa fa-newspaper-o"></i>
+			<input type="submit" value="Články">			
+		    </form>
+		</li>
+                <li>
+		    <form action="/monitoring.php" method="POST">
+			<input type="hidden" name="contentChange" value="2">
+			<i class="fa fa-address_card"></i>
+			<input type="submit" value="Recenze">			
+		    </form>
+		</li>
+               <li>
+		    <form action="/monitoring.php" method="POST">
+			<input type="hidden" name="contentChange" value="3">
+			<i class="fa fa-calendar-check-o"></i>
+			<input type="submit" value="Úkoly">			
+		    </form>
+		</li>
+            </ul>
+        </div>
+        <div id="content">
+                    <?php
+                        require("backend/connect.php");
+	
+			if(isset($_POST["contentChange"]))		
+				$content = $_POST["contentChange"];
+			else
+				$content = 0;
+
+			switch($content){
+				case 1:
+					
+					if($search != "")
+						$sql = "SELECT id_prispevku, titulek, tematicke_cislo, stav FROM prispevek WHERE CAST(id_prispevku AS varchar(10)) LIKE '" . $search . "%' OR titulek LIKE '%" . $search . "%' OR tematicke_cislo LIKE '%" . $search . "%' OR stav LIKE '%" . $search . "%'";
+					else
+						$sql = "SELECT id_prispevku, titulek, tematicke_cislo, stav FROM prispevek";
+          
+					$result = $conn->query($sql);
+            				$roles_array = get_roles();
+			
+					echo "<h2>Články</h2>					
+            		     		<div id='innercontent'>
+					<form action='monitoring.php' method='POST' id='searchForm' name='searchForm'>
+						<input type='hidden' name='contentChange' value='1'>
+						<i class='fa fa-search'></i>	
+						<input type='text' name='search' id='search' placeholder='ID/Titulek/Téma/Stav' value=$search>
+					</form>
+				  	<table>               
+                    			<tr id='tableheader'>
+                        					<th id='id'>ID</th>
+                        					<th id='title'>Titulek</th>
+                        					<th id='theme'>Téma</th>
+                        					<th id='state'>Stav</th>
+                   					</tr>";
+					
+					if ($result->num_rows > 0) {				
+			     			// Výpis článků
+						while($row = $result->fetch_assoc()) {
+							echo "<tr id='user_" .  $row["id_prispevku"] . "'>
+                                        			<td>". $row["id_prispevku"] . "</td>
+                                        			<td><a href='clanek.php?id=" .$row["id_prispevku"]."' target='_blank'>". $row["titulek"] . "</a></td>
+                                       				<td>". $row["tematicke_cislo"] . "</td>
+                                        			<td>". $row["stav"] . "</td>
+                                      </tr>";
+					echo "</table></div>";
+				break;
+					
+				case 2:					
+					if($search != "")
+						$sql = "SELECT * FROM recenze WHERE CAST(id_recenze AS varchar(10)) LIKE '" . $search . "%' OR recenze_text LIKE '%" . $search . "%'";
+					else
+						$sql = "SELECT * FROM recenze";
+					
+					$result = $conn->query($sql);
+					
+					echo "<h2>Recenze</h2>
+            		      			<div id='innercontent'>
+						<form action='monitoring.php' method='POST' id='searchForm' name='searchForm'>
+							<input type='hidden' name='contentChange' value='2'>
+							<i class='fa fa-search'></i>	
+							<input type='text' name='search' id='search' placeholder='ID/Titulek/Téma/Stav' value=$search>
+						</form>
+				 		 <table>               
+                    					<tr id='tableheader'>
+                        					<th id='id_review'>ID</th>
+                        					<th id='review_score'>Aktuálnost</th>
+                        					<th id='review_score'>Originalita</th>
+                        					<th id='review_score'>Odborná úroveň</th>
+                       						<th id='review_score'>Jazyková úroveň</th>
+                                  <th id='review_text'>Text recenze</th>
+                   					</tr>";
+					echo "</table></div>";
+				break;
+			}
+	
+                    ?>
+        </div>
+    </div>
+</body>
+</html>
