4791bb22e03c70821f8ce43f43d17e31beed4270
GRANDfaceStudent <matl07@student.vspj.cz>
2022-12-17 20:07:48 +0100

    Update articles_upload.php

--- projekt/backend/articles_upload.php
+++ projekt/backend/articles_upload.php
@@ -7,12 +7,26 @@
 
 	if(!empty($_POST["article_checked"]))
 	{
-		foreach($_POST["article_checked"] as $article_checked)
+		$articles_array = array();
+		$array_index = 0;
+		foreach($_POST["article_checked"] as $article_id)
 		{
-			
+			$select = "select soubor_cesta from soubor where id_prispevku=$article_id";
+			$result = mysqli_query($conn, $select);
+			$articles_array[array_index] = $result["soubor_cesta"];
+			array_index++;
 		}
 		$_SESSION["message"] = "Podařilo se zveřejnit článek.";
 		header("Location: ../index.php");
+		$fileArray= array("name1.pdf","name2.pdf","name3.pdf","name4.pdf");
+
+		$datadir = "save_path/";
+		$outputName = $datadir."merged.pdf";
+
+		$cmd = "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$outputName ";
+		//Add each pdf file to the end of the command
+		foreach($fileArray as $file) { $cmd .= $file." "; }
+		$result = shell_exec($cmd);
 	}
 	else
 	{
