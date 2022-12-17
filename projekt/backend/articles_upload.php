a78a530b8a27c47b4e81d2ea9ce0044ddc7bec5e
GRANDfaceStudent <matl07@student.vspj.cz>
2022-12-17 19:50:04 +0100

    Update articles_upload.php

--- projekt/backend/articles_upload.php
+++ projekt/backend/articles_upload.php
@@ -1,4 +1,11 @@
 <?php
+	//Session--------
+	session_start();
+
+	//Connect database--------
+	require("connect.php");
+
+	
 	$_SESSION["message"] = "Nepodařilo se zveřejnit článek.";
 	header("Location: ../index.php");
 ?>
