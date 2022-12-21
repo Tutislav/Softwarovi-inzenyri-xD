4b22a363905dca9518ebaeb7752a9c3ccc5a3754
pelika14 <115732648+pelika14@users.noreply.github.com>
2022-12-21 13:49:50 +0100

    Update real_approve.php

--- projekt/backend/real_approve.php
+++ projekt/backend/real_approve.php
@@ -2,7 +2,7 @@
 	if (strcmp($_POST["approve_submit"],"Schválit změnu")==0) {
 		$kladny=1;
 		echo "kladny";
-	} elseif (strcmp($_POST["dissapprove_submit"],"Zamítnout změnu")==0) {
+	} elseif (strcmp($_POST["disapprove_submit"],"Zamítnout změnu")==0) {
 		$zaporny=1;	
 		echo "zaporny";
 	}
