76622cc58ba4bca3e5f5efefcc32860fdf6edd10
pelika14 <115732648+pelika14@users.noreply.github.com>
2022-12-21 14:25:46 +0100

    Update real_approve.php

--- projekt/backend/real_approve.php
+++ projekt/backend/real_approve.php
@@ -1,16 +1,15 @@
 <?php
 	//funguje
 	if (strcmp($_POST["approve_submit"],"Schválit změnu")==0) {
-		$skutecny_soubor=$_POST["souborupraveny_id"];
-		echo $skutecny_soubor;
+		$sql = "UPDATE prispevek SET zobrazeny_soubor='$_POST["souborupraveny_id"]' WHERE id_prispevku='$article_id';";
 		
 	} elseif (strcmp($_POST["disapprove_submit"],"Zamítnout změnu")==0) {
-		$skutecny_soubor=$_POST["soubor_id"];	
-		echo $skutecny_soubor;
+		//$skutecny_soubor=$_POST["soubor_id"];	
+		$sql = "UPDATE prispevek SET zobrazeny_soubor='$_POST["soubor_id"]' WHERE id_prispevku='$article_id';";
 	}
 	//****
 	require("backend/connect.php");
-	echo $skutecny_soubor;
+	echo $sql;
         /*$sql = "UPDATE prispevek SET zobrazeny_soubor='$skutecny_soubor' WHERE id_prispevku='$article_id';";
         $result = $conn->query($sql);
         $conn->close();
