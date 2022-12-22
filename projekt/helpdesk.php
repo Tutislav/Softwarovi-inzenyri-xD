652c429ad02c7fd84bec463f969fa6f241024803
pelika14 <pelika14@student.vspj.cz>
2022-12-22 16:44:45 +0100

    Update helpdesk.php

--- projekt/helpdesk.php
+++ projekt/helpdesk.php
@@ -16,6 +16,9 @@
 	<style>
 	.content{
 		padding:15px;
+		width:64%;
+		float:left;
+		display:none;
 	}
 	</style>
 </head>
@@ -51,39 +54,52 @@
 				<p>Zpřístupnění recenzí</p>
 				<p>Komunikace s recenzenty</p>
 				<p>Schvalování drobných změn</p>
-                <p>Přidávání recenzí</p>
             </div>
-            <div class="content loginhelp" style="float:left;width:66%">
+            <div class="content loginhelp">
 Přihlášení uživatelů
 Jedná se o formulář, který slouží k přihlašování zaregistrovaných uživatelů. Obsahuje dvě textové pole: emailová adresa a heslo; a tlačítko pro přihlášení. Po odeslání formuláře se kontroluje, zda se údaje shodují se zápisy v databázi, jestliže ano, pak je uživatel úspěšně přihlášen, v opačném případě je uživatel přesměrován zpátky na úvodní stránku systému s určitou zprávou.
 
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
+			<div class="content showhelp">
+			Uživatel má možnost zobrazit si vydané články. Dále je zde možnost filtrování podle tématu. článek si zobrazí tak že klikne na odkaz články v navigaci.
 			
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content addhelp">
+			Autor má po přihlášení možnost přejít na stránku pro přidávání článků.
+Ta obsahuje formulář se vstupem pro titulek článku, spoluautory, samotný dokument článku s příponou .doc, .docx nebo .pdf, výběr ze čtyř témat a tlačítko na odeslání.
+Po vyplnění a stisknutí tlačítka se data z formuláře odešlou do souboru se skriptem, kde jsou data zpracována a následně vložena jako nový záznam do tabulky v databázi.
+
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content showreviewhelp">
+			Autor si může zobrazit recenze, nacházející se na spodku stránky u jeho zhodnocených článků.
+Každá recenze obsahuje jméno recenzenta, datum odeslání, hodnocení jednotlivých aspektů na stupnici od jedné po pět hvězd a tlačítko pro zobrazení celé recenze.
+To po kliknutí zobrazí text slovního zhodnocení a tlačítko odkazující na oponentní formulář, má-li autor k jakékoliv recenzi nějaké námitky.
+
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content formhelp">
+			Autor má možnost poslat námitky k recenzím jeho článků pomocí oponentního formuláře.
+Formulář obsahuje jméno článku, jméno recenzenta, textové pole pro námitky k recenzi a tlačítko k odeslání. Po odeslání se námitka k recenzi objeví ve vzkazech (podstránka webu), na které vidí redaktor.
+
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content reviewinghelp">
+			Recenzent má možnost si zobrazit přidělené články od redaktora, které má zrecenzovat, v podstránce „ČLÁNKY K RECENZI“.
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content statehelp">
+			Je místo kde redaktor může měnit stav článku a přidat k němu i nějaký vzkaz.
+obrázek ukazuje, jak se redaktor dostane k formuláři
+Vlastní stránka formuláře potom dává možnost nastavit stav příspěvku a případně napsat autorovi nějaké informace pomocí vzkazů. Zobrazuje se zde i název článku, aby redaktor věděl, ke kterému článku dělá.
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content reviewallowhelp">
+			Redaktor může zpřístupnit posudky recenzentů autorovi vybráním možnosti Zpřístupnit recenze.
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content comhelp">
+			Redaktor má možnost předat daným recenzentům určitý článek pomocí formuláře, na který se dostane přes: SPRÁVA ČLÁNKŮ > SPRAVOVAT > Předat recenzentům.
             </div>
-			<div class="content loginhelp" style="border-left:1px solid black;float:left;width:66%;display:none">
-			
+			<div class="content smallchangeapprovehelp">
+			Pokud se redaktorovi na článku něco nelíbí,  může si vyžádat od autora, aby svůj článek upravil.
+Po úpravě autorem se ve správě článků zobrazí tlačítko, kterým se redaktor dostane na stránku, na níž jsou odkazy jak na předchozí, tak i upravený článek.
+Tento upravený článek pak může pomocí tlačítek buď schválit, a nebo zamítnout.
+
             </div>
         </div>
     </div>
